75966a7cf9156cccc8c9d13fab666cf1bb319246
Александр Ковалев <a.kovalev@2gis.ru>
2022-11-03 08:04:37 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp11
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите необходимый вам разделитель");
+            var separator = Console.ReadLine();
+            var array = new int[10];
+            Random random = new Random();            
+            for ( int i = 0; i < array.Length;i++ ) {
+                array[i] = random.Next(0, 1000);
+            }
+            Console.WriteLine(array.SetSeparatorAndShowArray(x => x, separator));
+        }
+
+        private static string SetSeparatorAndShowArray<S, T>(this IEnumerable<S> array, Func<S, T> selector, 
+            string separator = ", ")
+        {
+            return string.Join(separator, array.Select(selector));        
+        }
+    }
+}
